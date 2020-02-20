3b35756fc5a3980e2898c027c36b99a2b17cc359
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-02-20 13:58:39 +0100

    Department domain model

---
+++ ConsoleApp/ConsoleApp/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+
+        
+
+    }
+}
