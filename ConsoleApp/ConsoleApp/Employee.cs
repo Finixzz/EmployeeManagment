602288de4ceee905ca47f7be49df879e190f1e05
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-02-20 13:58:50 +0100

    Employee domain model

---
+++ ConsoleApp/ConsoleApp/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public String FirstName{ get; set; }
+        public String LastName { get; set; }
+        public Department Department { get; set; }
+
+        public Employee(Department dept)
+        {
+            Department = dept;
+        }
+
+        private void setFirstName()
+        {
+            Console.Write("Input employees first name: ");
+            this.FirstName = Console.ReadLine();
+        }
+        private void setLastName()
+        {
+            Console.Write("Input employees last name: ");
+            this.LastName = Console.ReadLine();
+        }
+        private void setDepartment()
+        {
+
+        }
+
+        public void setEmployee()
+        {
+            this.setFirstName();
+            this.setLastName();
+            this.setDepartment();
+        }
+    }
+}
