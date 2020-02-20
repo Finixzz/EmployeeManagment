ade64c46770a0fd956e3bcd8e4cdf57740be951d
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-02-20 13:59:13 +0100

    Interfaces

---
+++ ConsoleApp/ConsoleApp/IDepartmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public interface IDepartmentRepository
+    {
+        Department GetDepartment(int id);
+        IEnumerable<Department> GetDepartments();
+
+    }
+}
---
+++ ConsoleApp/ConsoleApp/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetEmployee(int id);
+        IEnumerable<Employee> GetEmployees();
+        Employee CreateEmployee(Employee employee);
+        Employee EditEmployee(Employee employee);
+        Employee DeleteEmployee(int id);
+    }
+}
