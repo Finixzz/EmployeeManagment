7ab8fbc1464fe6f1adf1889034656df4105a0893
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-02-20 16:21:29 +0100

    Finished mockUpDept implementation

--- ConsoleApp/ConsoleApp/MockUpDepartmentRepository.cs
+++ ConsoleApp/ConsoleApp/MockUpDepartmentRepository.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleApp
 {
-    public class MockUpDepartmentRepository
+    public class MockUpDepartmentRepository : IDepartmentRepository
     {
+        private List<Department> departmentList;
+
+        public MockUpDepartmentRepository()
+        {
+            departmentList = new List<Department>()
+            {
+                new Department{Id=1,Name="None"},
+                new Department{Id=2, Name="IT"},
+                new Department{Id=3,Name="HR"},
+            };
+        }
+        public Department GetDepartment(int id)
+        {
+            Department departmentInRepo = departmentList.SingleOrDefault(c => c.Id == id);
+            if(departmentInRepo==null)
+            {
+                Console.WriteLine("Invalid department id!");
+                return null;
+            }
+            return departmentInRepo;
+        }
+
+        public IEnumerable<Department> GetDepartments()
+        {
+            return departmentList;
+        }
     }
 }
