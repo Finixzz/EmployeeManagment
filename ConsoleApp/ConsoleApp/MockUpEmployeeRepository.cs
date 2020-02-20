226a51735b8dc23780fc8c056e2b6d266eea79f8
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-02-20 13:59:48 +0100

    Mock up repositories that will implement interfaces

---
+++ ConsoleApp/ConsoleApp/MockUpEmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class MockUpEmployeeRepository
+    {
+        private List<Employee> employeeList;
+
+        public MockUpEmployeeRepository()
+        {
+            employeeList = new List<Employee>()
+            {
+                new Employee(new Department()){ Id=1,FirstName="Amet",LastName="Durmic"},
+
+
+            };
+        }
+    }
+}
